beb5fc4cd4bdb315e63f4497a134afcca8119f01
JoseMMorales <josemulamorales33@hotmail.com>
2021-03-04 13:22:54 +0100

    Adding searchCars query in CarsRepository

---
+++ src/Controller/CarsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\CarsRepository;
+
+class CarsController extends AbstractController
+{
+    /**
+     * @Route("/search/{brand}/{model}", 
+     *         name="route", 
+     *         methods={"GET"},
+     *         defaults={"brand" = 0, "model" = 0})
+     */
+    public function queryBuilder(string $brand, string $model, CarsRepository $carsRepository) : Response
+    // , string $seller = '', int $km = null,int $year = null, int $price = null, CarsRepository $carsRepository) : Response
+    {
+        $cars= $carsRepository->searchCars($brand, $model);
+        // , $seller, $km, $year, $price);
+        return new JsonResponse($cars);
+    }
+
+    /**
+     * @Route("/details/{id}", name="details", methods={"GET"})
+     */
+    public function detailsCars(int $id, CarsRepository $carsRepository) : Response
+    {
+        $car= $carsRepository->detailsCars($id);
+        return new JsonResponse($car);
+    }
+}
--- src/Repository/CarsRepository.php
+++ src/Repository/CarsRepository.php
@@ -19,6 +19,76 @@ class CarsRepository extends ServiceEntityRepository
         parent::__construct($registry, Cars::class);
     }
 
+    public function searchCars($brand, $model)
+    // , $seller, $km, $year, $price) 
+    {
+        $imageURL = "http://localhost:8000/img/";
+
+        // $brandDQL = 0;
+        // $variable = 0;
+
+        // ($model !== 0) ? $modelDQL = "AND m.modelName = '$model'" : $modelDQL = "WHERE m.modelName = '$model'";
+        
+        // return $this->getEntityManager()
+        //         ->createQuery(
+        //             "SELECT 
+        //                 c.carId as id,
+        //                 c.carYear as year, 
+        //                 c.km as km,
+        //                 c.shortDescription as description,
+        //                 c.carPrice as price,
+        //                 CONCAT('$imageURL' , c.mainImage) as image,
+        //                 m.modelName as model, 
+        //                 b.brandName as brand,
+        //                 u.userType as seller
+        //             FROM App:Cars c 
+        //             JOIN App:Users u WITH c.user = u.userId
+        //             JOIN App:Models m WITH c.model = m.id
+        //             JOIN App:Brands b WITH m.brand = b.id
+        //             WHERE $brandDQL = '$variable'")
+        //         ->getResult();
+
+        // $query = $this->createQueryBuilder('c')
+        //               ->from('App:Cars','c')
+        //               ->where('c.carId = 1');
+                      
+        // return($query);
+    }
+
+    public function detailsCars($id)
+    {
+        $imageURL = "http://localhost:8000/img/";
+        
+        return $this->getEntityManager()
+                        ->createQuery(
+                        "SELECT 
+                            c.carId as id,
+                            c.carYear as year, 
+                            c.km as km,
+                            c.shortDescription as description,
+                            c.longDescription as sellerDescription,
+                            c.carPrice as price,
+                            m.modelName as model, 
+                            b.brandName as brand,
+                            CONCAT('$imageURL' , c.mainImage) as mainImage,
+                            CONCAT('$imageURL' , c.secondImage) as secondImage,
+                            CONCAT('$imageURL' , c.thirdImage) as thirdImage,
+                            CONCAT('$imageURL' , c.fourthImage) as fourthImage,
+                            CONCAT('$imageURL' , c.fifthImage) as fifthImage,
+                            u.username as sellerName,
+                            u.userAddress as sellerAddress,
+                            u.userPhone as sellerPhone,
+                            u.userEmail as sellerEmail,
+                            u.userType as seller
+                        FROM App:Cars c 
+                        JOIN App:Users u WITH c.user = u.userId
+                        JOIN App:Models m WITH c.model = m.id
+                        JOIN App:Brands b WITH m.brand = b.id
+                        WHERE c.carId = '$id'")
+                        
+                        ->getResult();
+    }
+
     // /**
     //  * @return Cars[] Returns an array of Cars objects
     //  */
