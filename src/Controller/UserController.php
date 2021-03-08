8b61bd86a35c6e94dcac9f3fdbd321ad5b2d826f
JoseMMorales <josemulamorales33@hotmail.com>
2021-03-08 12:07:53 +0100

    Adding delete functionallity in UserController to delete User account in DDBB

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,13 @@ use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UsersRepository;
 use App\Entity\Users;
 
+/**
+ * @Route("user")
+ */
 class UserController extends AbstractController
 {
     /**
-     * @Route("user/data/{id}", name="data_user", methods={"GET"})
+     * @Route("/data/{id}", name="data_user", methods={"GET"})
      */
     public function user(int $id , UsersRepository $repo): Response
     {
@@ -26,7 +30,8 @@ class UserController extends AbstractController
             'email' => $user->getEmail(),
             'address' => $user->getAddress(),
             'city' => $user->getCity(),
-            'phone' => $user->getPhone()
+            'phone' => $user->getPhone(),
+            'seller' => $user->getType()
         ];
         // print_r($userObj['email']);
     
@@ -34,9 +39,9 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("user/add", name="add_user", methods={"POST","GET"})
+     * @Route("/add", name="add_user", methods={"POST","GET"})
      */
-    public function addUser(Request $request, EntityManagerInterface $em): Response
+    public function addUser(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
     {
         $username = $request->get('username');
         $email = $request->get('email');
@@ -46,8 +51,11 @@ class UserController extends AbstractController
         $user->setActive(1);
         $user->setName($username);
         $user->setEmail($email);
-        $user->setPassword($password);
         $user->setRoles([]);
+
+        $plainPassword = $password;
+        $encoded = $encoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encoded);
 
         $em->persist($user);
         $em->flush();
@@ -65,38 +73,80 @@ class UserController extends AbstractController
     }
 
     /**
-     * @Route("user/update/{id}", name="update_user", methods={"PUT","GET"})
+     * @Route("/update/{id}", name="update_user", methods={"PUT","GET"})
      */
-    public function updateUser(int $id, Request $request, UsersRepository $repo, EntityManagerInterface $em): Request
+    public function updateUser(int $id, Request $request, UsersRepository $repo, EntityManagerInterface $em): Response
     {
-        $username = $request->get('username');
-        $type = $request->get('type');
-        $email = $request->get('email');
-        $phone = $request->get('phone');
-        $address = $request->get('address');
-        $city = $request->get('city');
-        $password = $request->get('password');
+        $username = null;
+        // $type = 'Particular';
+        // $username = $request->get('username');
+        // $type = $request->get('type');
+        // $email = $request->get('email');
+        // $phone = $request->get('phone');
+        // $address = $request->get('address');
+        // $city = $request->get('city');
+        // $password = $request->get('password');
 
+        // $bodyRequest = $request->getContent();
+        // $userArray = json_decode($bodyRequest);
 
         $user = $repo->find($id);
-        $curso->setNombre($cursoArray -> nombre);
-    
-    
-        $user->setName('test');
-        $user->setEmail('test');
-        $user->setPassword('test');
+        // if ($username) {
+        //     echo($username);
+            $user->setName("Test");
+            // $user->setName($userArray -> username);
+        // };
 
+        // if ($type) {
+            // $user->setType("$type");
+            // $user->setType($userArray -> type);
+        // };
+
+        // if ($email) {
+        //     $user->setEmail("$email");
+        // }
+        // if ($phone) {
+        //     $user->setPhone("$phone");
+        // }
+        // if ($address) {
+        //     $user->setAddress("$address");
+        // }
+        // if ($city) {
+        //     $user->setCity("$city");
+        // }
+        // if ($password) {
+        //     $user->setPassword("$password");
+        // }
+    
         $em->persist($user);
         $em->flush();
 
-        $response = [];
-        $response['id'] = $user-> getUserId();
-        $response['name'] = $user->getName();
-        $response['email'] = $user->getEmail();
-        $response['address'] = $user->getAddress();
-        $response['city'] = $user->getCity();
-        $response['phone'] = $user->getPhone();
+        // $response = [];
+        // $response['id'] = $user-> getUserId();
+        // $response['name'] = $user->getName();
+        // $response['email'] = $user->getEmail();
+        // $response['address'] = $user->getAddress();
+        // $response['city'] = $user->getCity();
+        // $response['phone'] = $user->getPhone();
+        // $response['seller'] = $user->getType();
         
-        return new JsonResponse($response);
+        return new JsonResponse($user);
     }
+
+    /**
+     * @Route("/delete/{id}", name="delete_user", methods={"DELETE","GET"})
+     */
+    public function delete(int $id, UsersRepository $repo, EntityManagerInterface $em): Response
+    {
+        $user = $repo->find($id);
+        $userName = $user->getName();
+
+        $em->remove($user);
+        $em->flush();
+
+        $response = [ 'message' => "$userName" ];
+
+        return new JsonResponse($response); 
+    }
+
 }
