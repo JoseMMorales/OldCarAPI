c86a8928baadce4da42da08e2c313dcd76ad3c3b
JoseMMorales <josemulamorales33@hotmail.com>
2021-03-11 20:15:27 +0100

    Updating functionallity PublishController

--- src/Controller/FavouritesController.php
+++ src/Controller/FavouritesController.php
@@ -4,17 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\FavouritesRepository;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FavouritesController extends AbstractController
 {
     /**
-     * @Route("/favourites", name="favourites")
+     * @Route("/favourites", name="favourites", methods={"GET"})
      */
-    public function index(): Response
+    public function favourites(FavouritesRepository $repo): Response
     {
-        return $this->render('favourites/index.html.twig', [
-            'controller_name' => 'FavouritesController',
-        ]);
+        $car = $this->getUser();
+        
+        $userObj = [
+            'id' => $user-> getUserId(),
+            'name' => $user->getName(),
+            'email' => $user->getEmail(),
+            'address' => $user->getAddress(),
+            'city' => $user->getCity(),
+            'phone' => $user->getPhone(),
+            'type' => $user->getType()
+        ];
+    
+        return new JsonResponse($userObj);
     }
 }
--- src/Controller/PublishController.php
+++ src/Controller/PublishController.php
@@ -7,54 +7,64 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\BrandsRepository;
+use App\Repository\ModelsRepository;
+use App\Repository\UsersRepository;
 use App\Repository\CarsRepository;
-use App\Entity\Users;
-use App\Entity\Cars;
 use App\Entity\Models;
 use App\Entity\Brands;
-
+use App\Entity\Users;
+use App\Entity\Cars;
 
 class PublishController extends AbstractController
 {
     /**
      * @Route("/publish", name="publish", methods={"POST"})
      */
-    public function publish(Request $request, CarsRepository $repo, EntityManagerInterface $em): Response
+    public function publish(
+        Request $request, 
+        UsersRepository $repoUsers,
+        CarsRepository $repoCar,
+        ModelsRepository $repoModel, 
+        BrandsRepository $repoBrands,
+        EntityManagerInterface $em): Response
     {
         $username = $request->get('username');
-        $email = $request->get('email');
+        $newEmail = $request->get('email');
         $phone = $request->get('phone');
         $address = $request->get('address');
         $city = $request->get('city');
         $type = $request->get('type');
-
         $brand = $request->get('brand');
         $model = $request->get('model');
-
-        print_r($brand);
-
         $km = $request->get('km');
         $price = $request->get('price');
         $year = $request->get('year');
         $shortDescription = $request->get('shortDescription');
         $longDescription = $request->get('longDescription');
-        $photos = $request->files->get('files');
+        $photos[0] = $request->files->get('file0');
+        $photos[1] = $request->files->get('file1');
 
-        $user = new Users();
-        $user->setActive(0);
-        $user->setName($username);
-        $user->setEmail($email);
-        $user->setPhone($phone);
-        $user->setAddress($address);
-        $user->setCity($city);
-        $user->setType($type);
-        $user->setRoles([]);
+        dump($request->files->get('file'));
+        dump($photos);
 
-        $brand = new Brands();
-        $brand->setBrandName($brand);
+        $user = $repoUsers->findOneBy(['email' => $newEmail]);
 
-        $model = new Models();
-        $model->setModelName($model);
+        if ($user) {
+           $user->setEmail($newEmail);
+           $em->persist($user);
+        } else {
+            $user = new Users();
+            $user->setActive(0);
+            $user->setName($username);
+            $user->setEmail($newEmail);
+            $user->setPhone($phone);
+            $user->setAddress($address);
+            $user->setCity($city);
+            $user->setType($type);
+            $user->setRoles([]);
+            $em->persist($user);
+        }
         
         $car = new Cars();
         $car->setActive(0);
@@ -63,27 +73,50 @@ class PublishController extends AbstractController
         $car->setShortDescription($shortDescription);
         $car->setLongDescription($longDescription);
         $car->setCarPrice($price);
+        $car->setUser($user);
 
-        $em->persist($user);
-        $em->persist($brand);
-        $em->persist($model);
+        $newModel = $repoModel->findOneBy(['modelName' =>  $model]);
+
+        if($newModel){
+            $car->setModel($newModel);
+        } else {
+            $newModel = new Models();
+            $newModel->setModelName($model);
+            $car->setModel($newModel);
+            $em->persist($newModel);
+        }
+
+        $existBrand = $repoBrands->findOneBy(['brandName' => $brand]);
+
+        if($existBrand) {
+            $newModel->setBrand($existBrand);
+            $em->persist($newModel);
+        } else {
+            $newBrand = new Brands();
+            $newBrand->setBrandName($brand);
+            $newModel->setBrand($newBrand);
+            $em->persist($newBrand);
+        }
+        
         $em->persist($car);
         $em->flush();
-        $carId = $repo->getCarId();
+        $carId = $car->getCarId();
 
-        $photoName = 0;
+        $photoNames = [];
         $dir = $this->getParameter('photos_cars');
 
-        foreach ($files as $key => $file) {
-            $photoName = "$brand"."/$model"."/$carId"."/IMG$key.jpg";
-            $photos->move($dir, $photoName);
+        foreach ($photos as $key => $photo) {
+            $index = $key + 1; 
+            $photoName = "/$carId"."-IMG$index.jpg";
+            $photo->move($dir."$brand"."/$model", $photoName);
+            $photoNames[] = $photoName;
         };
 
         $car->setMainImage($photoName[0]);
         $car->setSecondImage($photoName[1]);
-        $car->setThirdImage($photoName[2]);
-        $car->getFourthImage($photoName[3]);
-        $car->setFifthImage($photoName[4]);
+        // $car->setThirdImage($photoName[2]);
+        // $car->setFourthImage($photoName[3]);
+        // $car->setFifthImage($photoName[4]);
 
         $em->persist($car);
         $em->flush();
