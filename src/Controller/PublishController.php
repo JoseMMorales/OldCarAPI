63553e4f5ff7b4e93bc8ccd68e18f55ef7ba58bd
JoseMMorales <josemulamorales33@hotmail.com>
2021-03-11 12:53:13 +0100

    Adding logic to PublishController POST

--- src/Controller/PublishController.php
+++ src/Controller/PublishController.php
@@ -7,13 +7,19 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CarsRepository;
+use App\Entity\Users;
+use App\Entity\Cars;
+use App\Entity\Models;
+use App\Entity\Brands;
+
 
 class PublishController extends AbstractController
 {
     /**
      * @Route("/publish", name="publish", methods={"POST"})
      */
-    public function publish(Request $request, EntityManagerInterface $em): Response
+    public function publish(Request $request, CarsRepository $repo, EntityManagerInterface $em): Response
     {
         $username = $request->get('username');
         $email = $request->get('email');
@@ -24,12 +30,15 @@ class PublishController extends AbstractController
 
         $brand = $request->get('brand');
         $model = $request->get('model');
+
+        print_r($brand);
+
         $km = $request->get('km');
         $price = $request->get('price');
         $year = $request->get('year');
         $shortDescription = $request->get('shortDescription');
         $longDescription = $request->get('longDescription');
-        $files = $request->files->get('files');
+        $photos = $request->files->get('files');
 
         $user = new Users();
         $user->setActive(0);
@@ -40,6 +49,12 @@ class PublishController extends AbstractController
         $user->setCity($city);
         $user->setType($type);
         $user->setRoles([]);
+
+        $brand = new Brands();
+        $brand->setBrandName($brand);
+
+        $model = new Models();
+        $model->setModelName($model);
         
         $car = new Cars();
         $car->setActive(0);
@@ -49,35 +64,31 @@ class PublishController extends AbstractController
         $car->setLongDescription($longDescription);
         $car->setCarPrice($price);
 
-        $em->persist($contact);
+        $em->persist($user);
+        $em->persist($brand);
+        $em->persist($model);
+        $em->persist($car);
+        $em->flush();
+        $carId = $repo->getCarId();
+
+        $photoName = 0;
+        $dir = $this->getParameter('photos_cars');
+
+        foreach ($files as $key => $file) {
+            $photoName = "$brand"."/$model"."/$carId"."/IMG$key.jpg";
+            $photos->move($dir, $photoName);
+        };
+
+        $car->setMainImage($photoName[0]);
+        $car->setSecondImage($photoName[1]);
+        $car->setThirdImage($photoName[2]);
+        $car->getFourthImage($photoName[3]);
+        $car->setFifthImage($photoName[4]);
+
+        $em->persist($car);
         $em->flush();
 
-        $mainFileName = 0;
-
-        if ($files[0]) {
-            $mainFileName = "$brand"."/$model"."/IMG1.jpg";
-        }
-        if ($files[1]) {
-            $mainFileName = "$brand"."/$model"."/IMG2.jpg";
-        }
-        if ($files[2]) {
-            $mainFileName = "$brand"."/$model"."/IMG1.jpg";
-        }
-        if ($files[3]) {
-            $mainFileName = "$brand"."/$model"."/IMG1.jpg";
-        }
-        if ($files[4]) {
-            $mainFileName = "$brand"."/$model"."/IMG1.jpg";
-        }
-
-        $car->setMainImage($files[0]);
-        $car->setSecondImage($files[1]);
-        $car->setThirdImage($files[2]);
-        $car->getFourthImage($files[3]);
-        $car->setFifthImage($files[4]);
-
-        $em->persist($contact);
-        $em->flush();
+        $response = ["Car and User uploaded onto DDBB"];
 
         return $this->json($response);
     }
